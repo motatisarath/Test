7a13f8427b62603fe64779d268fb9c52a86e854b
motatisarath <motatisarath.reddy@uipath.com>
2022-05-30 12:03:52 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,36 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <LightTheme>
+      <MenuList style={{
+    width: '250px'
+  }} id="menu-list-grow" disablePadding={true}>
+        <ListItem alignItems="flex-start" className="header">
+            Menu header
+        </ListItem>
+        <MenuItem>
+          <ListItemText primary="Default menu item" />
+        </MenuItem>
+        <MenuItem selected={true}>
+          <ListItemText primary="Selected menu item" />
+        </MenuItem>
+        <MenuItem disabled={true} divider={true}>
+          <ListItemText divider primary="Disabled menu item" />
+        </MenuItem>
+        <ListItem alignItems="flex-start" className="header">
+            Menu header
+        </ListItem>
+        <MenuItem>
+          <ListItemText primary="Default menu item" />
+        </MenuItem>
+        <MenuItem selected={true}>
+          <ListItemText primary="Selected menu item" />
+        </MenuItem>
+        <MenuItem disabled={true}>
+          <ListItemText primary="Disabled menu item" />
+        </MenuItem>
+      </MenuList>
+    </LightTheme>
     </div>
   );
 }
